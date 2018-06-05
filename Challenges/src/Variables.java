267be6b1b5451089fd6d83e6aed1dbf1ba57a516
Eddy Caldas <eddycaldas@live.com>
2018-06-04 21:23:49 -0600

    variables

--- Challenges/src/Variables.java
+++ Challenges/src/Variables.java
@@ -1,4 +1,17 @@
 
 public class Variables {
-
+	// Challenge #1:
+	// write a java program that reads a number in inches,
+	// converts it into meters.
+	// note: one inch is 0.0254 meter
+	// test data
+	// input a value in inch: 1000
+	// expect output:
+	// 1000.0 inches is 25.4 meters
+	
+	public static void main(String[] args) {
+		double inchVal = 1000;
+		double meterVal = (inchVal * 0.0254);
+		System.out.println(inchVal + " inches is " + meterVal + " meters.");
+	}
 }
